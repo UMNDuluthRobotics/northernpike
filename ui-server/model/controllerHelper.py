c866d0a55cf7c083bfafb39e8a2cd27ae52848ec
Joseph Hnatek <jhnatek07@gmail.com>
2019-04-19 16:00:25 -0500

    Create controllerHelper.py
    jsonify controller roll, pitch, and yaw values and send it to a websocket server.

---
+++ ui-server/model/controllerHelper.py
@@ -0,0 +1,41 @@
+import pygame
+import numpy as np
+import json
+import time
+
+from websocket import create_connection
+
+# -------- Main Program Loop -----------
+pygame.init()
+pygame.joystick.init()
+ws = create_connection("ws://192.168.43.1:12345")
+
+while True:
+    pygame.event.get()
+    joystick = pygame.joystick.Joystick(0)
+    joystick.init()
+
+    # Roll right / Roll left
+    roll = np.int8(joystick.get_axis(0) * 128)
+
+    # Up / Down
+    pitch = np.int8(joystick.get_axis(1) * 128)
+
+    # Turn right / Turn left
+    yaw = np.int8(joystick.get_axis(2) * 128)
+
+    # Store the values as a list of list. Otherwise json doesn't like int8 arrays
+    roll = roll.tolist()
+    pitch = pitch.tolist()
+    yaw = yaw.tolist()
+
+    j = json.dumps({"Roll": roll,
+                    "Pitch": pitch,
+                    "Yaw": yaw}
+                   , sort_keys=True, indent=4)
+
+    # Delay
+    time.sleep(.5)
+    ws.send(j)
+
+ws.close()
